6e159276ee1f8c8e56d2ea188b1e1dfcc2c67c65
Gods-Grad1 <155855705+Gods-Grad1@users.noreply.github.com>
2026-06-27 00:14:49 +0200

    Refactor comments and clean up wsgi.py
    Updated comments for clarity and removed unnecessary lines.

--- wsgi.py
+++ wsgi.py
@@ -1,35 +1,32 @@
-# wsgi.py
 import os
 import sys
 import threading
 
-# Disable all proxies (just in case)
+# Disable proxies
 os.environ['HTTP_PROXY'] = ''
 os.environ['HTTPS_PROXY'] = ''
 os.environ['http_proxy'] = ''
 os.environ['https_proxy'] = ''
 os.environ['NO_PROXY'] = '*'
 
-# Disable telebot's proxy
 from telebot import apihelper
 apihelper.proxy = None
 
-# Import your bot (this runs the bot.py file)
+# Import your bot
 import bot
 
-# Start the bot in a background thread
+# Delete any existing webhook to avoid conflict
+bot.bot.delete_webhook()
+
 def run_bot():
     bot.bot.infinity_polling()
 
 thread = threading.Thread(target=run_bot, daemon=True)
 thread.start()
 
-# Flask web server to keep Render alive and provide a status page
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Za Sora Bot is running! 🚀"
-
-# For Render, the entry point is this Flask app.
